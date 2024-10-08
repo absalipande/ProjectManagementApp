f3c6868f5d189a20724387bbe6ace58792ad4903
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-08 14:24:21 +0800

    feat: Created an endpoint for creating a new project and creating a correspoding route for it

--- backend/dist/src/controllers/projectController.js
+++ backend/dist/src/controllers/projectController.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getProjects = void 0;
+exports.createProject = exports.getProjects = void 0;
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
 const getProjects = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -18,7 +18,25 @@ const getProjects = (req, res) => __awaiter(void 0, void 0, void 0, function* ()
         res.status(200).json(projects);
     }
     catch (error) {
-        res.status(500).json({ message: 'An error occurred while fetching projects' });
+        res.status(500).json({ message: `An error occurred while fetching projects: ${error.message}` });
     }
 });
 exports.getProjects = getProjects;
+const createProject = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name, description, startDate, endDate } = req.body;
+    try {
+        const newProject = yield prisma.project.create({
+            data: {
+                name,
+                description,
+                startDate,
+                endDate,
+            },
+        });
+        res.status(201).json(newProject);
+    }
+    catch (error) {
+        res.status(500).json({ message: `An error occurred while creating a project: ${error.message}` });
+    }
+});
+exports.createProject = createProject;
