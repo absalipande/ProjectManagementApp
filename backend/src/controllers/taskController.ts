5ee015483f5fac7523d2e7953257ed083658d105
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-09 11:36:07 +0800

    feat: Created a post and patch endpoint for task feature as well as creating the corresponding route to it

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -24,3 +24,49 @@ export const getTasks = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ message: `An error occurred while fetching tasks: ${error.message}` });
   }
 };
+
+export const createTask = async (req: Request, res: Response): Promise<void> => {
+  const { title, description, status, priority, tags, startDate, dueDate, points, projectId, authorUserId, assignedUserId } = req.body;
+
+  try {
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        tags,
+        startDate,
+        dueDate,
+        points,
+        projectId,
+        authorUserId,
+        assignedUserId,
+      },
+    });
+
+    res.status(201).json(newTask);
+  } catch (error: any) {
+    res.status(500).json({ message: `An error occurred while creating a task: ${error.message}` });
+  }
+};
+
+export const updateTaskStatus = async (req: Request, res: Response): Promise<void> => {
+  const { taskId } = req.params;
+  const { status } = req.body;
+
+  try {
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: Number(taskId),
+      },
+      data: {
+        status: status,
+      },
+    });
+
+    res.json(updatedTask);
+  } catch (error: any) {
+    res.status(500).json({ message: `An error occurred while updating task status: ${error.message}` });
+  }
+};
