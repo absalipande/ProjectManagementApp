d608acceb463007e8067bf7d529d0a97f99a2876
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-10 10:35:53 +0800

    feat: Setup the types in the frontend

--- frontend/src/state/api.ts
+++ frontend/src/state/api.ts
@@ -1,10 +1,70 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-export const api = createApi({ 
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
-    reducerPath: 'api',
+export interface Project {
+    id: number;
+    name: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+}
+
+export enum Priority {
+    Urgent = "Urgent",
+    High = "High",
+    Medium = "Medium",
+    Low = "Low",
+    Backlog = "Backlog",
+}
+
+export enum Status {
+    ToDo = "ToDo",
+    WorkInProgress = "Work In Progress",
+    UnderReview = "Under Review",
+    Completed = "Completed",
+}
+
+export interface User {
+    userId?: number;
+    username: string;
+    email: string;
+    profilePictureUrl?: string;
+    cognitoId?: string;
+    teamId?: number;
+}
+
+export interface Attachment {
+    id: number;
+    fileUrl: string;
+    fileName: string;
+    taskId: number;
+    uploadedById: number;
+}
+
+export interface Chat {
+    id: number;
+    title: string;
+    description?: string;
+    status?: Status;
+    priority?: Priority;
+    tags?: string;
+    startDate?: string;
+    dueDate?: string;
+    points?: number;
+    projectId: number;
+    authorUserId?: number;
+    assignedUserId?: number;
+
+    author?: User;
+    assignee?: User;
+    comments?: Comment[];
+    attachments?: Attachment[];
+}
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+    reducerPath: "api",
     tagTypes: [],
     endpoints: (build) => ({}),
-})
+});
 
 export const {} = api;
