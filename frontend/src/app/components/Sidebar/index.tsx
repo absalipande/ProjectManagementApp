b11be59b913519c4cac6ba556b5a2e3e91dcda00
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-11 15:24:45 +0800

    feat: Partially implemented the Projects page

--- frontend/src/app/components/Sidebar/index.tsx
+++ frontend/src/app/components/Sidebar/index.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
-import { useAppDispatch, useAppSelector } from '@/app/redux';
-import { setIsSidebarCollapsed } from '@/state';
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
+import { useGetProjectsQuery } from "@/state/api";
 import {
   AlertCircle,
   AlertOctagon,
@@ -19,18 +20,21 @@ import {
   User,
   Users,
   X,
-} from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React, { useState } from 'react';
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 
 const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const { data: projects } = useGetProjectsQuery();
   const dispatch = useAppDispatch();
-  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
 
   const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
     transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
@@ -42,7 +46,9 @@ const Sidebar = () => {
       <div className="flex h-full w-full flex-col justify-start">
         {/* TOP LOGO */}
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
-          <div className="text-xl font-bold text-gray-800 dark:text-white">Minerva</div>
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            Minerva
+          </div>
           {isSidebarCollapsed ? null : (
             <button
               className="py-3"
@@ -59,7 +65,9 @@ const Sidebar = () => {
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
           <div>
-            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">Minerva Team</h3>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              Minerva Team
+            </h3>
             <div className="mt-1 flex items-start gap-2">
               <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
               <p className="text-xs text-gray-500">Private</p>
@@ -90,6 +98,17 @@ const Sidebar = () => {
           )}
         </button>
 
+        {/* PROJECTS LIST */}
+        {showProjects &&
+          projects?.map((project) => (
+            <SidebarLink
+              key={project.id}
+              icon={Briefcase}
+              href={`/projects/${project.id}`}
+              label={project.name}
+            />
+          ))}
+
         {/* PRIORITIES LINKS */}
         <button
           onClick={() => setShowPriority((prev) => !prev)}
@@ -104,11 +123,27 @@ const Sidebar = () => {
         </button>
         {showPriority && (
           <>
-          <SidebarLink icon={AlertCircle} label='Urgent' href='/priority/urgent' />
-          <SidebarLink icon={ShieldAlert} label='High' href='/priority/high' />
-          <SidebarLink icon={AlertTriangle} label='Medium' href='/priority/medium' />
-          <SidebarLink icon={AlertOctagon} label='Low' href='/priority/low' />
-          <SidebarLink icon={Layers3} label='Backlog' href='/priority/backlog' />
+            <SidebarLink
+              icon={AlertCircle}
+              label="Urgent"
+              href="/priority/urgent"
+            />
+            <SidebarLink
+              icon={ShieldAlert}
+              label="High"
+              href="/priority/high"
+            />
+            <SidebarLink
+              icon={AlertTriangle}
+              label="Medium"
+              href="/priority/medium"
+            />
+            <SidebarLink icon={AlertOctagon} label="Low" href="/priority/low" />
+            <SidebarLink
+              icon={Layers3}
+              label="Backlog"
+              href="/priority/backlog"
+            />
           </>
         )}
       </div>
@@ -124,18 +159,22 @@ interface SidebarLinkProps {
 
 const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
   const pathname = usePathname();
-  const isActive = pathname === href || (pathname === '/' && href === '/dashboard');
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
 
   return (
     <Link href={href} className="w-full">
       <div
-        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 
-          ${isActive ? 'bg-gray-100 text-white dark:bg-gray-600' : ''} justify-start px-8 py-3`}
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`}
       >
-        {isActive && <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />}
+        {isActive && (
+          <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
+        )}
 
         <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
-        <span className="font-medium text-gray-800 dark:text-gray-100">{label}</span>
+        <span className="font-medium text-gray-800 dark:text-gray-100">
+          {label}
+        </span>
       </div>
     </Link>
   );
--- frontend/src/app/dashboardWrapper.tsx
+++ frontend/src/app/dashboardWrapper.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useEffect } from "react";
-import Navbar from "./(components)/Navbar";
-import Sidebar from "./(components)/Sidebar";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
@@ -37,7 +37,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-        <DashboardLayout>{children}</DashboardLayout>
+      <DashboardLayout>{children}</DashboardLayout>
     </StoreProvider>
   );
 };
