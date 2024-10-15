a7f93066c4b67b94e124f5f8b9d3a79a8511752f
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-15 14:11:47 +0800

    feat: Partially created the modal for creating or adding new Project

--- frontend/src/app/projects/ProjectHeader.tsx
+++ frontend/src/app/projects/ProjectHeader.tsx
@@ -6,9 +6,11 @@ import {
   Grid3x3,
   Grid3X3,
   List,
+  PlusSquare,
   Share2,
   Table,
 } from "lucide-react";
+import ModalNewProject from "./ModalNewProject";
 
 type Props = {
   activeTab: string;
@@ -27,9 +29,22 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
 
   return (
     <div className="px-4 xl:px-6">
-      {/* MODAL NEW TASKS */}
+      <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewProjectOpen(false)}
+      />
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-        <Header name="Product Design Development" />
+        <Header
+          name="Product Design Development"
+          buttonComponent={
+            <button
+              onClick={() => setIsModalNewProjectOpen(true)}
+              className="hover:bg-blue-600 flex items-center rounded-md bg-blue-primary px-3 py-2 text-white"
+            >
+              <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+            </button>
+          }
+        />
       </div>
 
       {/* TABS */}
