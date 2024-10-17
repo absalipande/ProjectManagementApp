76be16b906817bdd8c9f1f3d42e5694959a5384e
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-17 17:24:17 +0800

    style: Adjusted the UI again

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -62,7 +62,7 @@ const Sidebar = () => {
         </div>
 
         {/* TEAM */}
-        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+        <div className="flex items-center gap-5 border-y-[1.5px] border-zinc-200 px-8 py-4 dark:border-zinc-700">
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
