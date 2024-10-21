67690b2cbd3dbf7759eebaa54eb1c8b976088823
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-21 10:45:51 +0800

    feat: Created a controller and routes for teams

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,7 @@ import projectRoutes from './routes/projectRoutes';
 import taskRoutes from './routes/taskRoutes';
 import searchRoutes from './routes/searchRoutes';
 import userRoutes from './routes/userRoutes';
+import teamRoutes from './routes/teamRoutes';
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -32,7 +33,8 @@ app.get('/', (req, res) => {
 app.use('/projects', projectRoutes);
 app.use('/tasks', taskRoutes);
 app.use('/search', searchRoutes);
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
+app.use('/teams', teamRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
