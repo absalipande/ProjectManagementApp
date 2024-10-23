83fd809b9939a53b18c79129f55849c1cf69d222
Amiel Brencis Salipande <absalipande@gmail.com>
2024-10-23 13:56:14 +0800

    feat: Created a new endpoint for fetching user tasks

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createTask, getTasks, updateTaskStatus } from '../controllers/taskController';
+import { createTask, getTasks, getUserTasks, updateTaskStatus } from '../controllers/taskController';
 
 const router = Router();
 
 router.get('/', getTasks);
+router.get('/user/:userId', getUserTasks);
 router.post('/', createTask);
-router.patch('/:taskId/status', updateTaskStatus)
+router.patch('/:taskId/status', updateTaskStatus);
 
 export default router;
